103042b9d5fbac56c43af1c543f1ae0478bc8557
Hung Hay Hat <diegocoshung19@gmail.com>
2024-03-26 22:02:02 +0700

    update sth

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,1 +1,12 @@
-alert('Hello')
+import { useState } from "react";
+
+const App = () => {
+    const a = useState(1);
+    return (
+        <input>
+        
+        </input>
+    );
+}
+
+export default App;
