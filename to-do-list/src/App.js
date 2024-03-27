bb39fa1563d5f93af9cf99eb1f72f94e40fc0e4d
Hung Hay Hat <diegocoshung19@gmail.com>
2024-03-27 14:22:14 +0700

    test

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,11 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Input from "./component/Input";
 
 const App = () => {
     const a = useState(1);
+    const [name, setName] = useState("");
+    const [names, setNames] = useState([]);
+
+    const handleOnChangeInput = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleAddNewTask = () => {
+        let arr = names;
+        arr.push(name);
+        setNames(arr);
+    }
+
+    
     return (
-        <input>
         
-        </input>
+        <>
+        {console.log("names", names)}
+            {
+                names?.length > 0 && names.map((item, index) => {
+                    console.log("render");
+                    return (
+                        <div key={index}>
+                            <div>{item}</div>
+                            <div>
+                                <button>Sửa</button>
+                                <button>Xóa</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+
+            <label>Tên công việc:</label>
+            <input type="text" name="desc" onChange={(event) => handleOnChangeInput(event)}/>
+            <button onClick={() => handleAddNewTask()}>Thêm mới</button>
+        </>
     );
 }
 
---
+++ to-do-list/src/component/Input.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+const Input = () => {
+  return (
+    <input></input>
+  )
+}
+
+export default Input
